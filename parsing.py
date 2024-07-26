af3d5fc0efe4071d27cd14bb17ac509fc88db804
Matthew Perkett <matt.perkett@gmail.com>
2024-07-25 20:07:52 -0600

    Add missing docstrings to parsing.py functions

--- parsing.py
+++ parsing.py
@@ -1,9 +1,14 @@
+"""
+Collection of functions to parse GRIT HTML files.
+"""
+
 from pathlib import Path
-from typing import Union
+from typing import Callable, Union
 
 import pandas as pd
 from lxml import etree
 
+# column names expected when parsing HTML
 EXPECTED_HEADER = [
     "Place",
     "Bib",
@@ -23,6 +28,7 @@ EXPECTED_HEADER = [
     "Run Crew Name",
 ]
 
+# column names to use
 REFORMATTED_HEADER = [
     "place",
     "bib",
@@ -43,7 +49,22 @@ REFORMATTED_HEADER = [
 ]
 
 
-def get_simple_value_handler(dtype):
+def get_simple_value_handler(
+    dtype: type,
+) -> Callable[[etree._Element], Union[None, int, float, str]]:
+    """
+    Return value handler for the provided type (int, float, str)
+
+    Args:
+        dtype (type): int, float, str
+
+    Raises:
+        ValueError: if dtype is not recognized
+
+    Returns:
+        Callable[[etree._Element], Union[None, int, float, str]]: function that processes an
+            etree._Element node and returns a value
+    """
     if dtype not in (int, float, str):
         raise ValueError(f"dtype ({dtype}) must be int, float, or str")
 
@@ -55,8 +76,21 @@ def get_simple_value_handler(dtype):
     return simple_value_handler
 
 
-def participant_name_handler(node):
-    # Example
+def participant_name_handler(node: etree._Element) -> str:
+    """
+    Process HTML for participant name
+
+    Args:
+        node (etree._Element): HTML node containing participantName__name node with top level
+            node <td class="ta-left">
+
+    Raises:
+        ValueError: if structure is not as expected
+
+    Returns:
+        str: participant name in the format f"{first_name} {last_name}"
+    """
+    # Example node to process
     # <td class="ta-left">
     #     <div class="participantName">
     #         <div class="participantName__image">
@@ -73,41 +107,59 @@ def participant_name_handler(node):
     if node.tag != "td":
         raise ValueError()
     if node.values() != ["ta-left"]:
-        raise ValueError()
+        raise ValueError(f"Expected node value 'ta-left', but got {node.values()}")
     if len(node) != 1:
-        raise ValueError()
+        raise ValueError(f"Expected node length == 1, but got {len(node)}")
 
     # <div class="participantName">
     pname_node = node[0]
     if pname_node.values() != ["participantName"]:
-        raise ValueError()
+        raise ValueError(f"Expected node value 'participantName', but got {pname_node.values()}")
     if len(pname_node) != 2:
-        raise ValueError()
+        raise ValueError(f"Expected node length == 2, but got {len(pname_node)}")
 
     # <div class="participantName__name">
     pname_name_node = pname_node[1]
     if pname_name_node.values() != ["participantName__name"]:
-        raise ValueError()
+        raise ValueError(
+            f"Expected node value 'participantName__name', but got {pname_name_node.values()}"
+        )
     if len(pname_name_node) != 2:
-        raise ValueError()
+        raise ValueError(f"Expected node length == 2, but got {len(pname_name_node)}")
 
     # <div class="participantName__name__firstName">Matthew</div>
     first_name_node = pname_name_node[0]
     if first_name_node.values() != ["participantName__name__firstName"]:
-        raise ValueError()
+        raise ValueError(
+            f"Expected node value 'participantName__name__firstName', but got {first_name_node.values()}"
+        )
     first_name = first_name_node.text
 
     # <div class="participantName__name__lastName">Perkett</div>
     last_name_node = pname_name_node[1]
     if last_name_node.values() != ["participantName__name__lastName"]:
-        raise ValueError()
+        raise ValueError(
+            f"Expected node value 'participantName__name__lastName', but got {last_name_node.values()}"
+        )
     last_name = last_name_node.text
 
     name = f"{first_name} {last_name}"
     return name
 
 
-def elevation_gain_handler(node):
+def elevation_gain_handler(node: etree._Element) -> float:
+    """
+    Process HTML node to get elevation gain in feet
+
+    Args:
+        node (etree._Element): HTML node with elevation (e.g. "<td>396ft (120.7m)</td>")
+
+    Raises:
+        ValueError: elevation gain text is not formatted as expected
+
+    Returns:
+        float: elevation_ft
+    """
     # Example:
     # <td>396ft (120.7m)</td>
     s = node.text
@@ -124,7 +176,13 @@ def elevation_gain_handler(node):
     return elevation_ft
 
 
-def get_handlers():
+def get_handlers() -> list[Callable]:
+    """
+    Return a handler to process the data node from each column of the table
+
+    Returns:
+        list[Callable]: list of handlers (one for each column)
+    """
     # TODO: parse clock_time, chip_time, pace into time or deltatime object
     # TODO: parse percent into a float instead of str
 
@@ -151,6 +209,18 @@ def get_handlers():
 
 
 def parse_grit_table_header(table_header: Union[etree._Element, None]) -> list[str]:
+    """
+    Parse the table header to get all the column names
+
+    Args:
+        table_header (Union[etree._Element, None]): node to parse (with tag == 'thead')
+
+    Raises:
+        ValueError: node is not in expected format
+
+    Returns:
+        list[str]: header
+    """
     if table_header is None:
         raise ValueError("table_header is None")
     if table_header.tag != "thead":
@@ -161,7 +231,23 @@ def parse_grit_table_header(table_header: Union[etree._Element, None]) -> list[s
     return header
 
 
-def parse_grit_table_body(table_body: Union[etree._Element, None], handlers) -> list[list[str]]:
+def parse_grit_table_body(
+    table_body: Union[etree._Element, None], handlers
+) -> list[list[Union[str, int, float]]]:
+    """
+    Parse the table body returning a list of lists with all of the data.
+
+    Args:
+        table_body (Union[etree._Element, None]): node to parse (with tag == 'tbody')
+        handlers (_type_): list of functions equal to the number of columns that handle parsing each
+            data node
+
+    Raises:
+        ValueError: node isn't formatted as expected
+
+    Returns:
+        list[list[str]]: data
+    """
     if table_body is None:
         raise ValueError(f"table_body is None")
     if table_body.tag != "tbody":
@@ -188,7 +274,19 @@ def parse_grit_table_body(table_body: Union[etree._Element, None], handlers) -> 
     return data
 
 
-def parse_grit_html(inp_file: Path):
+def parse_grit_html(inp_file: Path) -> pd.DataFrame:
+    """
+    Parse the GRIT HTML table node and build a Pandas dataframe with the results
+
+    Args:
+        inp_file (Path): saved HTML input file path
+
+    Raises:
+        ValueError: header does not match what was expected (maybe format has changed?)
+
+    Returns:
+        pd.DataFrame: df
+    """
     # read html file
     with open(inp_file, "r") as input_file:
         s = input_file.read()
